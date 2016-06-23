665aed56e47ce4f61931780948d1f81c5a0d65c4
Merlyn Albery-Speyer <merlyn@newrelic.com>
2016-06-22 22:24:30 -0700

    Better! 121 +/- 2.4.

--- tabular-sarsa-td-on-policy.py
+++ tabular-sarsa-td-on-policy.py
@@ -6,23 +6,32 @@ import random
 env = gym.make('MountainCar-v0')
 
 q = collections.defaultdict(lambda: np.zeros(3)) # zero is optimistic for MountainCar
-gamma = 0.995 # discounting factor (1.0 -> no discounting)
-alpha = 0.1 # learning rate
-epsilon = 0.1 # exploration factor (0.0 -> no exploration)
+gamma = 1.0 # discounting factor (1.0 -> no discounting)
+alpha = 0.3 # learning rate
+epsilon = 0.0 # exploration factor (0.0 -> no exploration)
+
+def normalize(high, low, value):
+    return int(45*(value-low)/(high-low))
 
 def state_key(state):
-    return "{},{}".format(round(state[0],1), round(state[1],1))
+    return "{},{}".format( \
+        normalize(env.observation_space.high[0], env.observation_space.low[0], state[0]), \
+        normalize(env.observation_space.high[1], env.observation_space.low[1], state[1]) \
+    )
 
-# env.monitor.start('/tmp/MountainCar-v0/experiment-5')
+env.monitor.start('/tmp/MountainCar-v0/experiment-6')
 
 sum = 0
 
-for i_episode in range(10000):
+# print(env.observation_space.high)
+# print(env.observation_space.low)
+
+for i_episode in range(15000):
     success = False
     state = env.reset()
     action = np.argmax(q[state_key(state)])
 
-    for t in range(1000):
+    for t in range(200):
         state_next, reward, done, _ = env.step(action)
         # if i_episode % 1000 == 99:
         #     env.render()
@@ -49,12 +58,12 @@ for i_episode in range(10000):
             break
 
     if not(success):
-        sum += 1000
+        sum += 200
 
-    if i_episode % 10 == 9:
-        print("{}: {}".format(i_episode+1, '#'*int((sum/10)/10)))
+    if i_episode % 100 == 99:
+        print("{}: {}{}     -- {}".format(i_episode+1, '#'*int((sum/100)/10), '-'*(20 - int((sum/100)/10)), len(q)))
         sum = 0
 
     # print("Episode {} finished".format(i_episode))
 
-# env.monitor.close()
+env.monitor.close()
